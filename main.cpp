0a71e11eb6fd67eaab8e65cc9678df31ea5eeb6c
makhambetaliev <r.maxambetaliev@newuu.uz>
2024-09-25 12:06:03 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    //Problem 1
+    // int x;
+    // x=0;
+    // while(x<10){
+    //     x=x+1;
+    //     cout<<x<<endl;
+    // }
+
+    //Problem 2
+    // int x,t;
+    // x = 0;
+    // t = 0;
+    // while(x < 10) {
+    //     x = x + 1;
+    //     t = t + x;
+    //     cout << x << endl;
+    // }
+    // cout << t << endl;
+
+    //Problem 3
+    // int x;
+    // x=0;
+    // while(x<100) {
+    //     x=x+1;
+    //     if(x%5!=0) {
+    //         cout<<x<<endl;
+    //     }
+    // }
+
+    //Problem 4
+    // int x,y;
+    // cin>>x;
+    // while(y<x) {
+    //     y=y+1;
+    //     if (y%10==0) {
+    //         cout<<y<<endl;
+    //     }
+    // }
+
+    //Problem 5
+    // int x,y;
+    // x=64;
+    // y=0;
+    // while(x<90) {
+    //     x=x+1;
+    //     y=y+1;
+    //     if (y>5) {
+    //         cout<<endl;
+    //         y = 1;
+    //     }
+    //     cout<<char(x)<<" ";
+    // }
+
+    //Problem 6
+    // double gpa=0;
+    // int cred = 0;
+    // int n;
+    // cin>>n;
+    // while(n--) {
+    //     double a,b;
+    //     cin>>a>>b;
+    //     gpa+=a*b;
+    //     cred += a;
+    // }
+    // cout<<"Your Total gpa is "<<gpa/cred<<endl;
+
+    //Problem 7
+    // int a;
+    // cin >> a;
+    // for (int i = 2; i * i <= a; ++i) {
+    //     if (a % i == 0) {
+    //         cout<<"not prime"<<endl;
+    //     }
+    //     else {
+    //         cout<<"prime"<<endl;
+    //     }
+    // }
+
+    //Problem 8
+    // int n,x,r;
+    // r=0;
+    // cin >> n;
+    // x=0;
+    // while(x!=n) {
+    //     x = x + 1;
+    //     r = r + (x*x);
+    // }
+    // cout << r << endl;
+
+    //Problem 9
+    // int x,m;
+    // m = -1;
+    // x = -1;
+    // while (x != 0) {
+    //     cin >> x;
+    //     if (x >= m) {
+    //         m = x;
+    //     }
+    // }
+    // cout << m << endl;
+
+    //Problem 10
+    // int x,m,p,n,z;
+    // p = 0;
+    // n = 0;
+    // m = 0;
+    // x = -1;
+    // z = 0;
+    // while (x != 0) {
+    //     z = z + 1;
+    //     cin >> x;
+    //     if (x <=0) {
+    //         n +=1;
+    //     }
+    //     else {
+    //         p += 1;
+    //     }
+    //     m = m + x;
+    //
+    // }
+    // cout <<"The number of positives is: " << p<<endl;
+    // cout <<"The number of negatives is: " << n-1<<endl;
+    // cout <<"The total is: " << m<<endl;
+    // cout <<"The average is: " << m/z<<endl;
+
+}
